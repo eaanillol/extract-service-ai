1f83ed9fec2cb85142098b4ceaf7bb84b22499d0
cpalenc <cpalenc@bancodebogota.com.co>
2025-06-01 20:17:33 -0500

    remove unused parameter

--- api_ocr.py
+++ api_ocr.py
@@ -50,7 +50,7 @@ async def extract_text(request: DocumentRequest):
             aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
             aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'),
             region_name=os.getenv('AWS_REGION', 'us-east-1'),
-            aws_session_token=os.getenv('AWS_SESSION_TOKEN')
+            # aws_session_token=os.getenv('AWS_SESSION_TOKEN')
         )
         
         # Llamar a Textract para detectar texto
